7833976a259521976929e8a78adc8f56b0d34ea4
DariaSidorko <daria.sidorko@gmail.com>
2025-08-08 19:43:08 -0400

    initial set up + register and log in set up

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const favicon = require('express-favicon');
 const logger = require('morgan');
 
+require('dotenv').config();
+console.log("MONGO_URI from env:", process.env.MONGO_URI); 
+const connectDB = require('./config/db');
+
 const mainRouter = require('./routes/mainRouter.js');
+const authRouter = require('./routes/authRoutes');
 
 // middleware
 app.use(cors());
@@ -14,7 +20,11 @@ app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
+// DB connection
+connectDB();
+
 // routes
 app.use('/api/v1', mainRouter);
+app.use('/api/v1/auth', authRouter);
 
 module.exports = app;
---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    role: {
+        type: String,
+        enum: ['mentor', 'student'],
+        required: true
+    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    avatarUrl: String,
+    bio: String,               // mentor-specific
+    zoomLink: String,          // mentor-specific
+    topics: [String],          // topics mentor can teach OR student studies
+    registeredSessions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Session' }],
+    notifications: [
+        {
+            message: String,
+            date: { type: Date, default: Date.now }
+        }
+    ]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);

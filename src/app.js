536234131797ff8a294c095f0e477bd7a1683770
DariaSidorko <daria.sidorko@gmail.com>
2025-08-12 22:04:43 -0400

    run prettier formating | app.js lines  order fixed

--- src/app.js
+++ src/app.js
@@ -4,13 +4,6 @@ const cors = require('cors');
 const favicon = require('express-favicon');
 const logger = require('morgan');
 
-require('dotenv').config();
-console.log('MONGO_URI from env:', process.env.MONGO_URI);
-const connectDB = require('./config/db');
-
-const mainRouter = require('./routes/mainRouter.js');
-const authRouter = require('./routes/authRoutes');
-
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -18,6 +11,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
+
+require('dotenv').config();
+console.log('MONGO_URI from env:', process.env.MONGO_URI);
+const connectDB = require('./config/db.js');
+
+const mainRouter = require('./routes/mainRouter.js');
+const authRouter = require('./routes/authRoutes');
 
 // DB connection
 connectDB();
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController.js');

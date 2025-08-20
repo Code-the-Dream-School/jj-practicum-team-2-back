42644ad1f479a27ad0af9786c011f4e5a94efe4e
DariaSidorko <daria.sidorko@gmail.com>
2025-08-20 18:12:25 -0400

    minor fixes

--- src/app.js
+++ src/app.js
@@ -11,9 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
-app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static('public'));
 
+if (!process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET not defined in .env');
+}
 app.use(cookieParser(process.env.JWT_SECRET));
 
 const connectDB = require('./config/db.js');
@@ -27,5 +30,16 @@ connectDB();
 // routes
 app.use('/api/v1', mainRouter);
 app.use('/api/v1/auth', authRouter);
+
+// 404 Not Found
+app.use((req, res, next) => {
+  return res.status(404).json({ message: 'Route not found' });
+});
+
+// 500 Server Error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  return res.status(500).json({ message: 'Server error' });
+});
 
 module.exports = app;

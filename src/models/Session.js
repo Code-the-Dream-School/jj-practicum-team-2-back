a6f003dc4ac854e139b69873facc2dc438897a9f
DariaSidorko <daria.sidorko@gmail.com>
2025-08-25 15:18:31 -0400

    Session Model

--- src/models/Session.js
+++ src/models/Session.js
@@ -1,39 +1,37 @@
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const sessionSchema = new Schema({
-  title: { 
-    type: String, 
-    required: true 
-},
+  title: {
+    type: String,
+    required: true,
+  },
   description: String,
-  classId: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'Class', 
-    required: true },
-  mentorId: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true 
-},
-  date: { 
-    type: Date, 
-    required: true 
-},
+  classId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Class',
+    required: true,
+  },
+  mentorId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   duration: Number,
-  type: { 
-    type: String, 
-    enum: ['lecture', 'qna'], 
-    required: true 
-},
-
+  type: {
+    type: String,
+    enum: ['lecture', 'qna'],
+    required: true,
+  },
   capacity: Number,
   participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   recordingLink: String,
-
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Session', sessionSchema);
